8d51a6854acbfe5a0017e68a8e29f591a99bbbcd
Gautameranpariya <ranpariyagautam23@gmail.com>
2025-09-09 22:40:43 +0530

    feat: [BOILER-1] Update Contact Details

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const ContactPage = () => {
-  return <div>Conact us page</div>
+  return (
+    <React.Fragment>
+      <p>Contact us page</p>
+      <p>If you have any questions, feel free to reach out!</p>
+    </React.Fragment>
+  )
 }
 
 export default ContactPage
